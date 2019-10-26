bdef7cd0a634248d739afc8acaeab73f0e9f8ada
106613 <106613@U-55182.domena.uksw.edu.pl>
2019-10-26 12:28:12 +0200

    Test

---
+++ Test/src/Hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Hello {
+	String str= "sfesgewf";
+	Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		String hello ="Hello world!";
+		System.out.print("Napis'Hello World' ma "+ hello.length()+ "znakow.");
+		
+	}
+}
